c874d72593dec9bf96522df97a01c8f5d9136e57
Hty <15161656368@163.com>
2025-07-09 20:50:45 +0800

    提交文件

---
+++ SpringBoot3/boot3-05-demo/src/main/java/com/hty/web/Boot305DemoApplication.java
@@ -0,0 +1,13 @@
+package com.hty.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot305DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot305DemoApplication.class, args);
+    }
+
+}
---
+++ SpringBoot3/boot3-05-demo/src/test/java/com/hty/web/Boot305DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hty.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot305DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
