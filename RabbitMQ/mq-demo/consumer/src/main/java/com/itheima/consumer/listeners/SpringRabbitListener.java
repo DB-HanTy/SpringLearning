5891c1bebad66a25dd923c4bba81ca1cda3fd2ef
Hty <15161656368@163.com>
2025-10-13 20:50:39 +0800

    提交文件
    Former-commit-id: 325c26453c95fda07fbdbf38ce227f6c9abd66c5

---
+++ RabbitMQ/mq-demo/consumer/src/main/java/com/itheima/consumer/listeners/SpringRabbitListener.java
@@ -0,0 +1,14 @@
+package com.itheima.consumer.listeners;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+//将当前类放到SpringIOC容器中
+@Component
+public class SpringRabbitListener {
+    //监听名称simple.queue队列，Spring只要接收到该队列的消息就会接收消息
+    @RabbitListener(queues = "simple.queue")
+    //Spring自动将接收的消息给方法参数msg
+    public void listenSimpleQueueMessage(String msg) throws InterruptedException {
+        System.out.println("spring 消费者接收到消息：【" + msg + "】");
+    }
+}
