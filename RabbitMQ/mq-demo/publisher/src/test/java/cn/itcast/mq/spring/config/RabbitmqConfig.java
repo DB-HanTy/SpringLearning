df5e6347a338afffab0e8d235797dfb16d2241ec
Hty <15161656368@163.com>
2025-10-13 19:53:19 +0800

    提交文件
    Former-commit-id: 5bb1c83e8cd97cee3484dedb7132a6a144c3573d

---
+++ RabbitMQ/mq-demo/publisher/src/test/java/cn/itcast/mq/spring/config/RabbitmqConfig.java
@@ -0,0 +1,55 @@
+package cn.itcast.mq.spring.config;
+
+import com.rabbitmq.client.ConnectionFactory;
+import org.junit.platform.commons.logging.LoggerFactory;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+
+import java.util.logging.Logger;
+
+@SpringBootConfiguration
+public class RabbitmqConfig {
+
+
+    private final Logger logger = (Logger) LoggerFactory.getLogger(this.getClass());
+
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+
+    @Value("${spring.rabbitmq.port}")
+    private int port;
+
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+    @Value("${spring.rabbitmq.virtual-host}")
+    private String virtualhost;
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory(host,port);
+        connectionFactory.setUsername(username);
+        connectionFactory.setPassword(password);
+        connectionFactory.setVirtualHost(virtualhost);
+        //connectionFactory.setPublisherConfirms(true);
+        return connectionFactory.getRabbitConnectionFactory();
+    }
+
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    //必须是prototype类型
+    public RabbitTemplate rabbitTemplate() {
+        RabbitTemplate template = new RabbitTemplate((org.springframework.amqp.rabbit.connection.ConnectionFactory) connectionFactory());
+        return template;
+    }}
+
+
+
