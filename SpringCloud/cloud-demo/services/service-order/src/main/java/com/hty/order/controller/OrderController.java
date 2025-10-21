1090003ce5859a24a17cee0256fa80bb3a6a1e7a
Hty <15161656368@163.com>
2025-10-21 21:17:37 +0800

    提交文件

--- SpringCloud/cloud-demo/services/service-order/src/main/java/com/hty/order/controller/OrderController.java
+++ SpringCloud/cloud-demo/services/service-order/src/main/java/com/hty/order/controller/OrderController.java
@@ -3,14 +3,29 @@ package com.hty.order.controller;
 import com.hty.order.bean.Order;
 import com.hty.order.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope//刷新配置
 @RestController
 public class OrderController {
     @Autowired
     OrderService orderService;
+
+    @Value("${order.timeout}")
+    String orderTimeout;
+
+    @Value("${order.auto-confirm}")
+    String orderAutoConfirm;
+
+    @GetMapping("/config")
+    public String config(){
+        return "order.timeout="+orderTimeout+": order.auto-confirm="+orderAutoConfirm;
+    }
+
     //创建订单
     @GetMapping("/create")
     public Order createOrder(@RequestParam("productId") Long productId,
