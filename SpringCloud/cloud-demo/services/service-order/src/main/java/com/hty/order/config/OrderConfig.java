d4fe820e5cb89890939d0122ab6c9af44eb651ca
Hty <15161656368@163.com>
2025-10-28 20:54:05 +0800

    提交文件

--- SpringCloud/cloud-demo/services/service-order/src/main/java/com/hty/order/config/OrderConfig.java
+++ SpringCloud/cloud-demo/services/service-order/src/main/java/com/hty/order/config/OrderConfig.java
@@ -1,6 +1,8 @@
 package com.hty.order.config;
 
 import feign.Logger;
+import feign.RetryableException;
+import feign.Retryer;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +10,11 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class OrderConfig {
+
+    @Bean
+    Retryer retryer(){
+        return new Retryer.Default();
+    }
 
     @Bean
     Logger.Level feignLoggerLevel(){
