5e197b7275c5a4109716613d13d4b13c414342bb
Hty <15161656368@163.com>
2025-11-12 13:39:45 +0800

    提交文件

---
+++ SpringCloud/cloud-demo/services/seata-order/src/main/java/com/hty/order/SeataOrderMainApplication.java
@@ -0,0 +1,24 @@
+package com.hty.order;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+
+@EnableFeignClients(basePackages = "com.hty.order.feign")
+@EnableTransactionManagement
+@MapperScan("com.hty.order.mapper")
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SeataOrderMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeataOrderMainApplication.class, args);
+    }
+
+
+}
---
+++ SpringCloud/cloud-demo/services/seata-storage/src/main/java/com/hty/storage/service/impl/StorageServiceImpl.java
@@ -0,0 +1,25 @@
+package com.hty.storage.service.impl;
+
+import com.hty.storage.mapper.StorageTblMapper;
+import com.hty.storage.service.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class StorageServiceImpl implements StorageService {
+
+    @Autowired
+    StorageTblMapper storageTblMapper;
+
+
+
+    @Transactional
+    @Override
+    public void deduct(String commodityCode, int count) {
+        storageTblMapper.deduct(commodityCode, count);
+        if (count == 5) {
+            throw new RuntimeException("库存不足");
+        }
+    }
+}
